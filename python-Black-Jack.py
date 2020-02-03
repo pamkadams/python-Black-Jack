7aa19f6add5f741047840e524ac7e9d6096d264f
Pam Adams <pkadams@epotential.com>
2020-02-03 10:32:34 -0500

    "deck class"

--- python-Black-Jack.py
+++ python-Black-Jack.py
@@ -57,66 +57,94 @@ class Player():
         :return: (boolean)
         """
 
-        if amount > self.balance:
-            print( f"Sorry you can not bet that much. You can only bet ${self.balance}.")
-            amount = int(input('Enter another bet'))
-            self.check_bet(amount)
-
-        if self.balance == 0:
-            print(f"Game over. Sorry you have run out of money.")
-            return play = False
+        if amount >= self.balance:
+            return False
         else:
-            return ('You have placed the bet ${amount}. May the cards be in your favor.')
+            return True
 
 
-    def value_of_hand(self, card1, card2):
-        for suit, face, value in card
-#I want value of each card and then sum it.
+    def value_of_hand(self, hand):
+        """
+        This method sums the value of the hand
+        :param
+        hand: (list) player's hand of cards
+        :return: (int)
+        """
+        sum = 0;
+        for card in hand:
+            sum += card[2]
+        return sum
 
 
-class Deck:
-""" The Deck object contains a deck of cards made of suits, faces and values
-- Ace is 1 here but will be tested in the program to see if 11 is better during play
+
+class Deck():
     """
-    suits = ["Diamond", "Club", "Heart", "Spade"]
-    faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    value = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+    The Deck object contains a deck of cards made of suits, faces and values
+    - Ace is 1 here but will be tested in the program to see if 11 is better during play
+    """
+
+
+    def __init__(self):
+        self.suits = ["Diamond", "Club", "Heart", "Spade"]
+        self.faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        self.value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 #generate a deck and return a shuffled deck to play
     def shuffled_deck(self):
-    """
-     This method creates and shuffles a deck of cards
+        """
+
+        This method creates and shuffles a deck of cards
         :param
 
         :return: (list)
-        """
+
+         """
         deck = []
-        for suit in suits:
-            for i in range(0, len(faces)+1):
-                card = (suit, faces[i], value[i])
+        for suit in self.suits:
+            for i in range(0, len(self.faces)):
+                card = (suit, self.faces[i], self.value[i])
                 deck.append(card)
-            shuffled_deck = random.sample(deck, k = 52)
-            return shuffled_deck
+        random_deck = random.sample(deck, k=52)
+        return random_deck
 
 
 def main():
 
     #setup game - find out player's name and amount with which they wish to gamble
     print("Welcome to Black Jack!")
-    player1 = input("What is your name?")
-    bankroll = int(input("How many chips in dollars do you want to buy?")
+    player1 = input("What is your name? ")
+    bankroll = int(input("How many chips in dollars do you want to buy? "))
     card_shark = Player(player1, bankroll)
+    computer = 'dealer'
+    dealer = Player(computer, 10000000)
 
-    #control flow
+    #manage game flow
     play = True
 
     while play:
+        if card_shark.balance == 0:
+            play = False
+
     #take bet
-        bet = int(input("How much do you want to bet?"))
+        bet = int(input("How much do you want to bet? "))
         #verify player has enough money to bet
-        card_shark.check_bet(bet)
+        valid_bet = card_shark.check_bet(bet)
+        while  not valid_bet:
+            print(f"Sorry you can not bet that much. You can only bet ${card_shark.balance}.")
+            bet = int(input('Enter another bet'))
+            card_shark.check_bet(bet)
+        print (f'You have placed the bet ${bet}. May the cards be in your favor.')
 
     #get deck and deal/print result fo deal
+        deck = Deck()
+        shoe_deck = deck.shuffled_deck()
+        card_shark.hand.append(shoe_deck.pop())
+       # dealer.hand.append(shoe_deck.pop())
+        #card_shark.hand.append(shoe_deck.pop())
+        print('dealer:', dealer.hand)
+        print('cardshark', card_shark.hand)
+
+
 
     #input from user 3rd card or not
 
@@ -126,5 +154,7 @@ def main():
 
     #win conditions
 
-if__name__='__main__':
+    print("Game over. Sorry you have run out of money.")
+
+if __name__ =='__main__':
     main()
